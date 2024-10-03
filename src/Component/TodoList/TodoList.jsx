7ce939b33e6260bf669ef48bfcc463cbdb61a318
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2024-10-03 14:36:10 +0300

    cascsac

--- src/Component/TodoList/TodoList.jsx
+++ src/Component/TodoList/TodoList.jsx
@@ -20,7 +20,7 @@ const dispatch = useAppDispatch()
     console.log(todolists)
     const addTodoList = (e) =>  APITodolist.setTodoList(e).then(res => {
         mas = res.data
-        //setmas(mas)
+        //setmas(mas)cascascsc
 //dcasascsacsqwdqwdwqdcascsaccsacs
         //console.log(res.data)
     })
